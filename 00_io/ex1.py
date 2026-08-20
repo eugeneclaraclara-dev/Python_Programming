ed86bc814da2528f015150862ea0332a59a20141
EugeneLee <eugene.clara.clara@dimigo.hs.kr>
2026-08-20 10:37:01 +0900

    add 00_io ex1.py

---
+++ 00_io/ex1.py
@@ -0,0 +1,36 @@
+# 입출력
+
+a = input()
+print(a, end="\n\n")
+print(type(a))
+print(a, type(a), sep="\n\t")
+
+a = int(a)
+print(type(a))
+
+a = int(input())
+print(a, "type:", type(a))
+
+b = float(input())
+print(b, "type:", type(b))
+
+# 정수 2개 입력
+# 100
+# 200
+
+a = int(input())
+b = int(input())
+
+# print(a, b)
+
+a, b, c = input().split()
+print(a, type(a))
+
+# map
+# map(함수, List 객체)
+a, b, c = map(int, input().split())
+print(a, b, c)
+
+# 리스트 변환
+a = list(map(int, input().split()))
+print(a, type(a))
